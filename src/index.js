1fc69c270ca39b883eca98ce20e4387bb553b87a
JuanD <judorozcocl29@gmail.com>
2022-09-24 09:44:35 -0500

    configuracion modulo app

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-//import express from 'express'
-const express = require("express")
-const morgan = require("morgan")
-
-const app = express()
-
-app.use(morgan("dev"))
-
-app.get('/', (req, res) => {
-    res.json("Get all")
-})
-
-app.get('/:id', (req, res) => {
-    res.json("Get by id")
-})
-
-app.post('/', (req, res) => {
-    res.json("Create by POST")
-})
-
-app.delete('/:id', (req, res) => {
-    res.json("Delete by DELETE")
-})
-
-app.put('/:id', (req, res) => {
-    res.json("Update by PUT")
-})
+const app = require("./app")
 
 app.listen(3000, () => {
     console.log("Server run")
