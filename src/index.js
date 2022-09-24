376e719c11312c710bd6a80fe59e2428493205af
JuanD <judorozcocl29@gmail.com>
2022-09-24 09:41:02 -0500

    implementando metodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 //import express from 'express'
 const express = require("express")
+const morgan = require("morgan")
 
 const app = express()
 
+app.use(morgan("dev"))
+
 app.get('/', (req, res) => {
-    res.send("Hola Mundo")
+    res.json("Get all")
+})
+
+app.get('/:id', (req, res) => {
+    res.json("Get by id")
+})
+
+app.post('/', (req, res) => {
+    res.json("Create by POST")
+})
+
+app.delete('/:id', (req, res) => {
+    res.json("Delete by DELETE")
+})
+
+app.put('/:id', (req, res) => {
+    res.json("Update by PUT")
 })
 
 app.listen(3000, () => {
